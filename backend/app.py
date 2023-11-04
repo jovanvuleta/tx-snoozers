b0ccc7f288065ff9a45784584d5e5529110d204f
jovanvuleta <jocavuleta@gmail.com>
2023-11-04 22:03:59 +0000

    fix: cors

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from backend.config import initiate_database
 from backend.helpers.logger import get_logger
@@ -9,6 +10,19 @@ load_dotenv()
 app = FastAPI()
 
 logger = get_logger()
+
+origins = [
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
